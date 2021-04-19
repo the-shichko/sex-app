3c0d017288f5f81e16111a249f5fdcf688e1f61b
the-shichko <pashashichko0075@gmail.com>
2021-04-19 19:08:30 +0300

    [feat] base files

---
+++ sex-app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using sex_app.Service;
+
+namespace sex_app
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var bot = new TelegramService();
+            bot.Start();
+
+            Console.WriteLine("Working...");
+            await Task.Delay(-1);
+        }
+    }
+}
---
+++ sex-app/Service/TelegramService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace sex_app.Service
+{
+    public class TelegramService
+    {
+        private const string Token = "1714106940:AAGcEPU6IOVlIb5oOg4YaN6aJDtkbFS9MZg";
+        private readonly TelegramBotClient _botClient;
+        private static readonly CommandService CommandService = new();
+
+        public TelegramService(string token = null)
+        {
+            _botClient = new TelegramBotClient(token ?? Token);
+        }
+
+        public void Start()
+        {
+            _botClient.StartReceiving();
+            _botClient.OnMessage += OnTelegramMessage;
+            
+            CommandService.InitCommands(_botClient);
+        }
+
+        private static async void OnTelegramMessage(object sender, MessageEventArgs e)
+        {
+            await CommandService.Execute(e.Message.Chat.Id, e);
+        }
+    }
+}
