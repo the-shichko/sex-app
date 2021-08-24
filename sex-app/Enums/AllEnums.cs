f51b5e357d2323f751df5d2d076385aef95825b5
the-shichko <pashashichko0075@gmail.com>
2021-08-24 23:57:42 +0300

    [feat] new poses

--- sex-app/Enums/AllEnums.cs
+++ sex-app/Enums/AllEnums.cs
@@ -4,116 +4,78 @@ namespace sex_app.Enums
 {
     public enum Location
     {
-        [Display(Name = "Мужчина сверху")]
-        MenOnTop,
-        [Display(Name = "Женщина сверху")]
-        WomenOnTop,
-        // [Display(Name = "Мужчина сзади")]
-        // MenBehind,
+        [Display(Name = "Мужчина сверху")] MenOnTop,
+        [Display(Name = "Женщина сверху")] WomenOnTop,
+        [Display(Name = "Мужчина сзади")] MenBehind,
+
         // [Display(Name = "Реверс")]
         // Reverse,
-        // [Display(Name = "Стоя")]
-        // StandingUp,
-        [Display(Name = "Лежа (муж.)")]
-        MenLyingDown,
-        [Display(Name = "Сидя (муж.)")]
-        MenSitting,
-        [Display(Name = "Лежа (жен.)")]
-        WomenLyingDown,
-        [Display(Name = "Лицо к лицу")]
-        FaceToFace,
-        [Display(Name = "Наездница")]
-        Rider,
-        [Display(Name = "Муж. на коленях")]
-        MenOnKnees,
-        [Display(Name = "Жен. на коленях")]
-        WomenOnKnees,
-        [Display(Name = "Doggy style")]
-        DoggyStyle,
-        [Display(Name = "Ноги на плечах")]
-        FeetOnShoulders
+        [Display(Name = "Стоя (муж.)")] MenStandingUp,
+        [Display(Name = "Стоя (жен.)")] WomenStandingUp,
+        [Display(Name = "Лежа (муж.)")] MenLyingDown,
+        [Display(Name = "Сидя (муж.)")] MenSitting,
+        [Display(Name = "Лежа (жен.)")] WomenLyingDown,
+        [Display(Name = "Лицо к лицу")] FaceToFace,
+        [Display(Name = "Наездница")] Rider,
+        [Display(Name = "Муж. на коленях")] MenOnKnees,
+        [Display(Name = "Жен. на коленях")] WomenOnKnees,
+        [Display(Name = "Doggy style")] DoggyStyle,
+        [Display(Name = "Ноги на плечах")] FeetOnShoulders
     }
 
     public enum Stimulation
     {
-        [Display(Name = "Точка P")]
-        DotP,
-        [Display(Name = "Клитор")]
-        Clitoris,
-        [Display(Name = "Точка A")]
-        DotA,
-        [Display(Name = "Точка G")]
-        DotG
+        [Display(Name = "Точка P")] DotP,
+        [Display(Name = "Клитор")] Clitoris,
+        [Display(Name = "Точка A")] DotA,
+        [Display(Name = "Точка G")] DotG
     }
 
     public enum LevelPenetration
     {
-        [Display(Name = "Без проникновения")]
-        Without,
-        [Display(Name = "Мелкое")]
-        Petty,
-        [Display(Name = "Среднее")]
-        Middle,
-        [Display(Name = "Глубокое")]
-        High
+        [Display(Name = "Без проникновения")] Without,
+        [Display(Name = "Мелкое")] Petty,
+        [Display(Name = "Среднее")] Middle,
+        [Display(Name = "Глубокое")] High
     }
 
     public enum AdditionalCaress
     {
-        [Display(Name = "Мять её попку")]
-        CrushHerAss,
-        [Display(Name = "Играть с её анусом")]
-        PlayWithHerAnus,
-        [Display(Name = "Мять её грудь")]
-        CrushHerBoobs,
-        [Display(Name = "Целоваться")]
-        Kiss,
-        [Display(Name = "Обниматься")]
-        ToCuddle,
-        [Display(Name = "Целовать грудь")]
-        KissBoobs
+        [Display(Name = "Мять её попку")] CrushHerAss,
+        [Display(Name = "Играть с её анусом")] PlayWithHerAnus,
+        [Display(Name = "Мять её грудь")] CrushHerBoobs,
+        [Display(Name = "Целоваться")] Kiss,
+        [Display(Name = "Обниматься")] ToCuddle,
+        [Display(Name = "Целовать грудь")] KissBoobs
     }
 
     public enum Activity
     {
-        [Display(Name = "Мужчина")]
-        Men,
-        [Display(Name = "Женщина")]
-        Women,
-        [Display(Name = "Оба")]
-        Both
+        [Display(Name = "Мужчина")] Men,
+        [Display(Name = "Женщина")] Women,
+        [Display(Name = "Оба")] Both
     }
 
     public enum BaseBool
     {
         Yea,
-        [Display(Name = "Нет")]
-        No,
-        [Display(Name = "Возможен")]
-        MayBe
+        [Display(Name = "Нет")] No,
+        [Display(Name = "Возможен")] MayBe
     }
 
     public enum Level
     {
-        [Display(Name = "Умеренный")]
-        Mild,
-        [Display(Name = "Сложный")]
-        Complicated,
-        [Display(Name = "Легкий")]
-        Easy
+        [Display(Name = "Умеренный")] Mild,
+        [Display(Name = "Сложный")] Complicated,
+        [Display(Name = "Легкий")] Easy
     }
-    
+
     public enum Category
     {
-        [Display(Name = "Куни")]
-        Cunnilingus,
-        [Display(Name = "Оральный секс")]
-        OralSex,
-        [Display(Name = "Поза 69")]
-        Position69,
-        [Display(Name = "Минет")]
-        Blowjob,
-        [Display(Name = "Секс")]
-        Sex,
+        [Display(Name = "Куни")] Cunnilingus,
+        [Display(Name = "Оральный секс")] OralSex,
+        [Display(Name = "Поза 69")] Position69,
+        [Display(Name = "Минет")] Blowjob,
+        [Display(Name = "Секс")] Sex,
     }
 }
--- sex-app/Models/ListPositions.cs
+++ sex-app/Models/ListPositions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Newtonsoft.Json;
 using sex_app.Enums;
 using sex_app.Extensions;
 
@@ -11,141 +13,49 @@ namespace sex_app.Models
     {
         public ListPositions(IEnumerable<PositionItem> items)
         {
-            // AddRange(items);
+            AddRange(items);
             AddRange(new List<PositionItem>
             {
                 new()
                 {
-                    Activity = Activity.Men,
-                    Categories = new List<Category> { Category.Sex },
-                    Level = Level.Easy,
-                    Locations = new List<Location> { Location.FaceToFace, Location.MenOnTop },
-                    Title = "Missionary (Миссионерская)",
-                    Text =
-                        "Миссионерская позиция, вероятно, является наиболее распространенной "
-                        + "первой позой, которую люди пробуют, что, вероятно, связано с ее простотой, " +
-                        "удобством и высоким уровнем близости. Чтобы принять это положение, женщина просто ложится на спину, " +
-                        "а мужчина лежит на ней лицом вниз. Хотя первоначальное прицеливание может быть для мужчины поначалу немного трудным, " +
-                        "получить от нее руку помощи можно довольно легко решить эту проблему. Мы рекомендуем",
-                    FileName = "missionary.png"
-                },
-                new()
-                {
-                    Activity = Activity.Men,
-                    Categories = new List<Category> { Category.Sex },
-                    Level = Level.Easy,
-                    Locations = new List<Location> { Location.DoggyStyle, Location.MenOnKnees, Location.WomenOnKnees },
-                    Title = "Doggy (Собачка)",
-                    Text =
-                        "Стандартный вариант стиля Doggy Style удобен, интуитивно понятен и универсален; женщина просто становится на четвереньки, а мужчина садится сзади и становится на колени. Как и в большинстве положений входа сзади, угол проникновения, как правило, очень хорошо подходит для нацеливания на точку G, поэтому мы рекомендуем женщинам изучить, как наклонять таз и выгибать спину, чтобы найти лучшие углы. Как и во всех положениях на коленях, мы рекомендуем положить подушки на пол для дополнительного комфорта, а также для точного выравнивания паха.",
-                    FileName = "doggy.png"
-                },
-                new()
-                {
-                    Activity = Activity.Women,
-                    Categories = new List<Category> { Category.Sex },
-                    Level = Level.Easy,
-                    Locations = new List<Location> { Location.WomenOnTop, Location.MenLyingDown, Location.FaceToFace },
-                    Title = "Cowgirl (Постушка)",
-                    Text =
-                        "В очень популярной позе наездницы мужчина лежит на спине, а женщина склоняется над ним лицом к его голове. Большая часть движений происходит от женщины, поднимающей, покачивающей и / или вращающей бедра, хотя мужчина может помочь придать вещам немного аромата снизу. Игра с ее грудями - одна из замечательных особенностей этой позы, хотя просто наблюдать, как они подпрыгивают, тоже не так уж плохо, просто не поддавайтесь гипнозу! Если вы на вершине, вы можете поэкспериментировать как с откидыванием назад, как показано на рисунке, так и с наклоном вперед на груди вашего партнера, чтобы найти то, что лучше всего подходит для вас.",
-                    FileName = "cowgirl.png"
-                },
-                new()
-                {
-                    Activity = Activity.Women,
-                    Categories = new List<Category> { Category.Sex },
-                    Level = Level.Easy,
-                    Locations = new List<Location> { Location.WomenOnTop, Location.MenLyingDown, Location.FaceToFace },
-                    Title = "Collapsed Cowgirl (Свернувшаяся пастушка)",
-                    Text =
-                        "Вариация свернутой наездницы - это в значительной степени именно то, на что это похоже. Хотите ли вы зайти поцеловаться или просто дать ногам немного отдохнуть, это отлично подходит для обоих. Движение для этой позиции обычно является командным усилием, поскольку ни один из партнеров не обладает большой подвижностью в этом раскладе. Предостережение для мужчин: будьте осторожны с зубами при выполнении более сильных ударов. Выбор времени во время поцелуя может иметь болезненные и дорогостоящие последствия! Вам также может понравиться подложить подушку под спину мужчине, чтобы наклонить его таз для более глубокого проникновения.",
-                    FileName = "cowgirl2.png"
-                },
-                new()
-                {
-                    Activity = Activity.Men,
-                    Categories = new List<Category> { Category.Sex },
-                    Level = Level.Easy,
-                    Locations = new List<Location> { Location.DoggyStyle, Location.MenOnKnees, Location.WomenOnKnees },
-                    Title = "Face Down Doggy (Собачка лицом вниз)",
-                    Text =
-                        "В варианте «Собачий стиль» лицом вниз женщина наклоняется вперед и кладет голову на пол. Угол проникновения не идеален для попадания в точку G, но отлично подходит для удара в малоизвестное место во влагалище, называемое задним сводом, а некоторые могут даже обнаружить усиленную стимуляцию клитора. Подушки рекомендуются под ее голову, а также под колени обоих людей, что не должно вызывать удивления, поскольку мы довольно много заботимся о комфорте. ",
-                    FileName = "doggy2.png"
-                },
-                new()
-                {
-                    Activity = Activity.Men,
-                    Categories = new List<Category> { Category.Sex },
-                    Level = Level.Easy,
-                    Locations = new List<Location>
-                        { Location.FaceToFace, Location.MenLyingDown, Location.WomenLyingDown, Location.MenOnTop },
-                    Title = "Tucked Missionary",
-                    Text =
-                        "Близкий родственник Folded Missionary, вариант Tucked отличается тем, что ноги женщины согнуты в коленях и расположены снаружи тела мужчины. Это позволяет снизить нагрузку на мужчин, которые слишком сильно наклоняются, и намного легче добиться интимной близости без ног. Мы рекомендуем женщинам поэкспериментировать как с углом таза, так и с тем, насколько открыты ноги, чтобы найти то, что чувствует себя лучше всего. ",
-                    FileName = "missionary3.png"
-                },
-                new()
-                {
-                    Activity = Activity.Men,
-                    Categories = new List<Category> { Category.OralSex, Category.Cunnilingus },
-                    Level = Level.Easy,
-                    Locations = new List<Location>
-                        { Location.MenLyingDown, Location.WomenLyingDown },
-                    Title = "Eagle (Орел)",
-                    Text =
-                        "В (раскинутой) позе орла женщина лежит на спине, поставив ступни, а мужчина лежит лицом вниз между ее ног. Эта поза пользуется большим успехом из-за высокого уровня комфорта, но может быть немного сложно добавить аппликатуру из-за ограниченного пространства для локтей ... Тем не менее, ее грудь находится в пределах досягаемости ... что всегда является преимуществом. Вы также можете подумать о подушке под ее спиной, чтобы наклонить ее таз для лучшего доступа к ротовой полости и уменьшить нагрузку на шею мужчины.",
-                    FileName = "cunnilingus.png"
-                },
-                new()
-                {
                     Activity = Activity.Women,
                     Categories = new List<Category> { Category.OralSex, Category.Blowjob },
-                    Level = Level.Easy,
+                    Level = Level.Mild,
                     Locations = new List<Location>
-                        { Location.MenLyingDown, Location.WomenOnKnees },
-                    Title = "Usual (Обычно)",
+                        { Location.MenOnKnees, Location.WomenLyingDown },
+                    Title = "Plumber Sex",
                     Text =
-                        "Вероятно, самый распространенный способ исполнения фелляции, в «Обычном» есть много чего. В него легко попасть, очень удобно, и это не сильно напрягает женскую шею. Это положение можно удерживать в течение длительного периода, прежде чем потребуется сделать перерыв или сменить положение, особенно при смене рук по мере необходимости. Единственным реальным недостатком является то, что распущенные длинные волосы могут попасть в рот, поэтому мужчинам следует подумать о помощи, когда это необходимо. ",
-                    FileName = "blowjob.png"
+                        "Эта поза, получившая свое название от сходства с сантехником, залезающим под раковину, является чрезвычайно удобным способом для женщин согреть трубку. Когда мужчина стоит на четвереньках, у женщин есть отличный доступ для некоторого времени, проведенного в тишине и яичках, а также для изучения некоторых менее посещаемых дорог, хотя, вероятно, было бы неплохо постучать, прежде чем их заклеймят как нарушителя. Однако есть небольшой недостаток, или, скорее, неглубокий, так как совмещение ее рта и горла не идеально для глубокого глотания. ",
+                    FileName = "blowjob4.png"
                 },
                 new()
                 {
-                    Activity = Activity.Both,
-                    Categories = new List<Category> { Category.OralSex, Category.Position69 },
-                    Level = Level.Easy,
+                    Activity = Activity.Men,
+                    Categories = new List<Category> { Category.Sex },
+                    Level = Level.Mild,
                     Locations = new List<Location>
-                        { Location.MenLyingDown, Location.WomenOnKnees },
-                    Title = "69",
+                        { Location.MenStandingUp, Location.WomenStandingUp, Location.DoggyStyle, Location.MenBehind },
+                    Title = "Supported Standing Doggy",
                     Text =
-                        "Оригинальная поза 69, которую легко выполнять, остается одной из самых популярных поз 69. В стандартном варианте мужчина ложится на спину, а его партнер становится на колени перед его лицом и наклоняется вперед. Подушка под голову мужчине - это необходимое условие, если вы собираетесь провести какое-то время в этой позе. ",
-                    FileName = "69.png"
-                },
-                new()
-                {
-                    Activity = Activity.Women,
-                    Categories = new List<Category> { Category.OralSex, Category.Blowjob },
-                    Level = Level.Easy,
-                    Locations = new List<Location>
-                        { Location.MenLyingDown, Location.WomenLyingDown },
-                    Title = "Open Usual",
-                    Text =
-                        "Открытый Обычный - это тонкая вариация Обычного, где мужчина раздвигает ноги, а женщина лежит между ними. Несмотря на небольшую разницу на бумаге, это оказывает огромное влияние на возможное количество яичек и анальной игры. Хорошей идеей будет иметь несколько подушек под спиной мужчины, чтобы наклонить его таз и еще больше улучшить доступ.",
-                    FileName = "blowjob2.png"
+                        "Популярный многими вариант «Собачка» с опорой на стойку - это универсальная поза, которую большинство из нас будет часто посещать во время своей сексуальной карьеры. Особенно хорошо подходит для людей с проблемами колен, женщины, вероятно, сочтут этот вариант намного более комфортным при более сильных толчках, что намного проще с повышенной подвижностью. Небольшую регулировку высоты можно выполнить, слегка согнув колени или поставив ступни вместе, хотя предпочтительнее иметь ступенчатый стул или высокие каблуки.",
+                    FileName = "doggy3.png"
                 },
                 new()
                 {
                     Activity = Activity.Women,
-                    Categories = new List<Category> { Category.OralSex, Category.Blowjob },
-                    Level = Level.Easy,
+                    Categories = new List<Category> { Category.Sex },
+                    Level = Level.Mild,
                     Locations = new List<Location>
-                        { Location.MenSitting, Location.WomenOnKnees },
-                    Title = "Sit & Blow",
+                        { Location.MenLyingDown, Location.WomenOnKnees, Location.Rider },
+                    Title = "Rodeo",
                     Text =
-                        "Sit & Blow - отличный способ заставить любого мужчину почувствовать себя королем замка ... и мы хотели бы думать, что это был любимый способ Билла произнести некоторые слова в овале. Легко попасть и довольно удобно, единственное, что мы действительно можем сказать, помимо шуток, - это то, что некоторые подушки на полу - это всегда умная игра, когда мы стоим на коленях.",
-                    FileName = "blowjob3.png"
+                        "Во главе семейства Rodeo стандартный вариант - это классика, которая нравится большинству людей. В него очень легко попасть; мужчина просто лежит на спине, а женщина становится на колени поверх него, отвернувшись. Предлагая большую подвижность, глубину и комфорт, он также намного более устойчив к выскальзыванию из-за слишком большого бокового движения - хотя чрезмерное вытягивание может быть довольно болезненным для мужчин, поэтому женщины будьте осторожны! Мы рекомендуем женщинам поэкспериментировать, положив руки на ноги переднего мужчины, или отказаться от рук, чтобы найти то, что доставит вам наибольшее удовольствие!",
+                    FileName = "sex2.png"
                 }
             });
+
+            // var json = JsonConvert.SerializeObject(this);
+            // File.WriteAllText($"{Directory.GetCurrentDirectory()}\\Data\\positions.json", json);
         }
     }
 
